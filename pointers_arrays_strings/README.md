6ace59774cd275d6bdf38dec92c867436b7d2ffe
aeodogbor <aeodogbor@gmail.com>
2022-10-06 21:00:19 -0700

    Task 5

---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Program on pointers, arrays, and strings
---
+++ pointers_arrays_strings/change_val.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+/**
+ * change_val - changes the value
+ * @n: integer to be changed 72
+ *
+ * Return: void
+ */
+void change_val(int *n)
+{
+	*n = 72;
+}
